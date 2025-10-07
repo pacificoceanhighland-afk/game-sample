10ee132f3869406b59ba5a1f85c36a45cbcc8aca
pacificoceanhighland-afk <pacificoceanhighland@gmail.com>
2025-10-07 15:50:01 +0900

    npc.js

---
+++ data/npc.js
@@ -0,0 +1,66 @@
+// NPCの位置とセリフ
+window.npcs = [
+  {
+    x: 5, y: 4,
+    map: palaceInside,
+    name: "兵士",
+    color: "#daa520",
+    image: "images/npc1.png",
+    imageRight: -250,
+    imageSize: 1000,
+    imageBottom: -320,
+    nameColor: "#daa520",
+    dialogue: [
+      { speaker: "npc", text: "おはようございます、陛下！"},
+      { speaker: "player", text: "うむ。今朝もご苦労であるな。"},
+      { speaker: "npc", text: "ありがとうございます。"},
+      { speaker: "npc", text: "陛下。"},
+      { speaker: "player", text: "どうした？"},
+      { speaker: "npc", text: "アレキサンドリア様が、「陛下がお目覚めになられたら、私のところに来るよう伝えてください」とのことです。"},
+      { speaker: "player", text: "そうなのか？"},
+      { speaker: "npc", text: "はい。"},
+      { speaker: "player", text: "ふむ。では、あやつは今どこにおる？"},
+      { speaker: "npc", text: "宮殿の外で待っていると、仰っていました。"},
+      { speaker: "player", text: "分かった。すぐに向かおう。"},
+      { speaker: "npc", text: "お気を付けて。"}
+    ]
+  },
+  {
+    x: 11, y: 2,
+    map: palaceOutside,
+    name: "アレキサンドリア",
+    color: "#00bfff",
+    image: "images/npc2.png",
+    imageRight: -120,
+    imageSize: 1000,
+    imageBottom: -500,
+    nameColor: "#00bfff",
+    dialogue: [
+      { speaker: "npc", text: "おはようございます、陛下。お待ちしておりました。"},
+      { speaker: "player", text: "うむ。"},
+      { speaker: "player", text: "宮殿内の兵士から、そなたのもとへと来るよう言われたのだ。"},
+      { speaker: "npc", text: "はい。"},
+      { speaker: "player", text: "して、こんな朝から余に何の用なのだ？"},
+      { speaker: "npc", text: "……まさか、お忘れなのですか？"},
+      { speaker: "player", text: "何が？"},
+      { speaker: "npc", text: "本当に、お忘れなのですね……。"},
+      { speaker: "player", text: "だから、その中身を今聞いておるのではないか！"},
+      { speaker: "npc", text: "はぁ……。"},
+      { speaker: "npc", text: "……。"},
+      { speaker: "npc", text: "今日は神殿に参る日と、以前に仰っていたではありませんか？"},
+      { speaker: "player", text: "……何？"},
+      { speaker: "npc", text: "ですから、今日は神殿への物資を持って行く日だと、申しているのです。"},
+      { speaker: "player", text: "……。"},
+      { speaker: "player", text: "…………。"},
+      { speaker: "player", text: "そうであった！"},
+      { speaker: "npc", text: "やっと思い出されましたか。"},
+      { speaker: "player", text: "ああ、こうしてはおれん！　今すぐに向かうぞ！"},
+      { speaker: "npc", text: "……本当、陛下は陛下ですね。"},
+      { speaker: "npc", text: "貴方様が幼い頃より仕えてきた身分ゆえ、今さらのことではありますが……。"},
+      { speaker: "npc", text: "既に神殿への連絡は済ませてありますので、後は御身が向かうだけとなっております。"},
+      { speaker: "player", text: "流石、余の臣下であるな！　主君のことを分かっておる！"},
+      { speaker: "npc", text: "……急ぎ向かってくださいませ。私もお供します。"},
+      { speaker: "player", text: "うむ、余の愛するあやつが待っておるのだ。早く行かねばな！"}
+    ]
+  }
+];
